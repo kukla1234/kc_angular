89f75fed31580fc504825af75d69da128781330d
ELizabeth Kukla <elizabethkukla@ELizabeths-MacBook-Pro.local>
2018-04-21 22:43:59 -0500

    fresh yo angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keelyCulbertsonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the keelyCulbertsonApp
+ */
+angular.module('keelyCulbertsonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keelyCulbertsonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the keelyCulbertsonApp
+ */
+angular.module('keelyCulbertsonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
